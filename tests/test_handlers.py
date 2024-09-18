01ae7a5787202c8f2094be81514df60480952b56
sskywwalkerr <radmir_rysaev1@mail.ru>
2024-09-18 12:39:46 +0500

    tests

---
+++ tests/test_handlers.py
@@ -0,0 +1,23 @@
+import json
+
+
+async def test_create_user(client, get_user_from_databse):
+    user_data = {
+        "name": "Radmir",
+        "surname": "Rysaev",
+        "email": "radmir@mail.ru"
+    }
+    resp = client.post("/user/", data=json.dumps(user_data))
+    data_from_resp = resp.json()
+    assert resp.status_code == 200
+    assert data_from_resp["name"] == user_data["name"]
+    assert data_from_resp["surname"] == user_data["surname"]
+    assert data_from_resp["email"] == user_data["email"]
+    user_from_db = await get_user_from_databse(data_from_resp["user_id"])
+    assert len(user_from_db) == 1
+    user_from_db = dict(user_from_db[0])
+    assert user_from_db["name"] == user_data["name"]
+    assert data_from_resp["surname"] == user_data["surname"]
+    assert user_from_db["email"] == user_data["email"]
+    assert user_from_db["is_active"] is True
+    assert str(user_from_db["user_id"]) == data_from_resp["user_id"]
