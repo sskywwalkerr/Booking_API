f87034b2b582b0f5ca25cd1c5e41a48a8cb8c69b
sskywwalkerr <radmir_rysaev1@mail.ru>
2024-09-25 14:02:29 +0500

    test, models, handlers, dals, conftest

--- api/models.py
+++ api/models.py
@@ -1,8 +1,9 @@
 import re
 import uuid
+from typing import Optional
 
 from fastapi import HTTPException
-from pydantic import BaseModel, EmailStr, validator
+from pydantic import BaseModel, EmailStr, validator, constr
 
 #########################
 # BLOCK WITH API MODELS #
@@ -47,5 +48,32 @@ class UserCreate(BaseModel):
             )
         return value
 
+
 class DeleteUserResponse(BaseModel):
     deleted_user_id: uuid.UUID
+
+
+class UpdatedUserResponse(BaseModel):
+    updated_user_id: uuid.UUID
+
+
+class UpdateUserRequest(BaseModel):
+    name: Optional[constr(min_length=1)]
+    surname: Optional[constr(min_length=1)]
+    email: Optional[EmailStr]
+
+    @validator("name")
+    def validate_name(cls, value):
+        if not LETTER_MATCH_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422, detail="Name should contains only letters"
+            )
+        return value
+
+    @validator("surname")
+    def validate_surname(cls, value):
+        if not LETTER_MATCH_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422, detail="Surname should contains only letters"
+            )
+        return value
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 from typing import Generator, Any
+from uuid import UUID
+
 import pytest
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
@@ -9,9 +11,7 @@ import os
 import asyncio
 from db.session import get_db
 import asyncpg
-from sqlalchemy import text
 import sys
-
 
 if sys.platform == 'win32':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
@@ -36,7 +36,7 @@ def event_loop():
 @pytest.fixture(scope="session", autouse=True)
 async def run_migrations():
     os.system("alembic init migrations")
-    os.system('alembic revision --autogenerate -m "test"')
+    os.system('alembic revision --autogenerate -m "test running migrations"')
     os.system("alembic upgrade heads")
 
 
@@ -54,7 +54,8 @@ async def clean_tables(async_session_test):
         async with session.begin():
             for table_for_cleaning in CLEAN_TABLES:
                 await session.execute(f"""TRUNCATE TABLE {table_for_cleaning};""")
-#await session.execute(text(f'TRUNCATE TABLE {table_for_cleaning};'))
+
+
 async def _get_test_db():
     try:
         yield test_async_session()
@@ -68,12 +69,9 @@ async def client() -> Generator[TestClient, Any, None]:
     the `get_db` dependency that is injected into routes.
     """
 
-
     app.dependency_overrides[get_db] = _get_test_db
     with TestClient(app) as client:
         yield client
-
-
 
 
 @pytest.fixture(scope="session")
@@ -81,7 +79,6 @@ async def asyncpg_pool():
     pool = await asyncpg.create_pool("".join(settings.TEST_DATABASE_URL.split("+asyncpg")))
     yield pool
     pool.close()
-
 
 
 @pytest.fixture
@@ -93,9 +90,12 @@ async def get_user_from_database(asyncpg_pool):
 
     return get_user_from_database_by_uuid
 
+
 @pytest.fixture
-async def create_user_in_database(user_id:str, name: str, surname: str, email: str, is_active: bool):
-    async with asyncpg_pool.acquire() as connection:
-        return await connection.execute("""INSERT INTO users ($1, $2, $3, $4, $5)""",
-                                        user_id, name, surname, email, is_active)
+async def create_user_in_database(asyncpg_pool):
+
+    async def create_user_in_database(user_id: str, name: str, surname: str, email: str, is_active: bool):
+        async with asyncpg_pool.acquire() as connection:
+            return await connection.execute("""INSERT INTO users VALUES ($1, $2, $3, $4, $5)""",
+                                            user_id, name, surname, email, is_active)
     return create_user_in_database
