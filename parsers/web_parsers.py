824b4dcb2ac42ee7a75c04dfa185c186a9720330
sskywwalkerr <radmir_rysaev1@mail.ru>
2024-11-15 13:36:30 +0500

    new function get_json and get_page

--- parsers/web_parsers.py
+++ parsers/web_parsers.py
@@ -1,17 +1,44 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
+# Функции парсинга конкретных веб-страниц
+import requests
+import json
 
-#  Функции  парсинга  конкретных  веб-страниц
+headers = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36",
+    "bx-ajax": "True"
+}
 
 
-def parse_product_page(url: str):
-    driver = webdriver.Chrome()
-    driver.get(url)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
+def get_page(url):
+    s = requests.Session()
+    response = s.get(url=url, headers=headers)
 
-    # Извлечение  данных  с  веб-страницы
-    title = soup.find('h1', class_='product-title').text.strip()
-    price = soup.find('span', class_='product-price').text.strip()
+    # Проверяем статус ответа
+    response.raise_for_status()
 
-    driver.quit()
-    return {"title": title, "price": price}
+    with open("index.html", "w", encoding="utf-8") as file:
+        file.write(response.text)
+
+
+def get_json(url):
+    s = requests.Session()
+    response = s.get(url=url, headers=headers)
+
+    try:
+        response.raise_for_status()
+        data = response.json()
+    except ValueError as e:
+        print("Error loading JSON: " + str(e))
+        print("Response content: " + response.text)
+    else:
+        with open("result.json", "w", encoding="utf-8") as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
+
+
+def main():
+    #get_page(url="https://www.lamoda.ru/c/15/shoes-women/")
+    get_json(url="https://www.lamoda.ru/api/v1/catalog/discovery")
+
+
+if __name__ == '__main__':
+    main()
