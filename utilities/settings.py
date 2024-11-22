463f145f7b8fb2dcfe8cd0a4c051e9320d75e9e4
sskywwalkerr <radmir_rysaev1@mail.ru>
2024-11-22 14:44:17 +0500

    new connect for redis

--- utilities/settings.py
+++ utilities/settings.py
@@ -1,6 +1,9 @@
 """settings  and configs  for the project"""
+import os
 
 from envparse import Env
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Optional
 
 env = Env()
 
@@ -19,4 +22,16 @@ TEST_DATABASE_URL = env.str(
     "TEST_DATABASE_URL",
     default="postgresql+asyncpg://postgres_test:postgres_test@127.0.0.1:5433/postgres_test"
 )# connect string for the database
+
+
+class Settings(BaseSettings):
+    database_url: str = os.environ.get("DATABASE_URL", "postgresql+asyncpg://postgres:postgres@127.0.0.1:5432/postgres") #Настройка вашей базы данных
+    redis_host: str = os.environ.get("REDIS_HOST", "localhost")  # Хост Redis
+    redis_port: int = int(os.environ.get("REDIS_PORT", 6379))  # Порт Redis
+    redis_db: int = int(os.environ.get("REDIS_DB", 0))  # Номер базы данных Redis
+    secret_key: str = os.environ.get("SECRET_KEY","21sdfas1") #Добавьте секретный ключ!
+
+    model_config = SettingsConfigDict(env_file=".env") #Читаем переменные окружения из .env
+
+settings = Settings()
 
