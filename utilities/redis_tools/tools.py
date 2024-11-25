89ee7bba19442f0cbe053208f1ff464997bb1f14
sskywwalkerr <radmir_rysaev1@mail.ru>
2024-11-25 15:07:04 +0500

    tools for redis

--- utilities/redis_tools/tools.py
+++ utilities/redis_tools/tools.py
@@ -1,21 +1,11 @@
 import redis
+from utilities.settings import settings
 
 
-class RedisTools:
-
-    __redis_connect = redis.Redis(host='redis', port=6379)
-
-    @classmethod
-    def set_pair(cls, name: str, item_basePrice: str):
-
-        cls.__redis_connect.delete(name, item_basePrice)
-
-    @classmethod
-    def get_pair(cls, name):
-
-        return cls.__redis_connect.get(name)
-
-    @classmethod
-    def get_keys(cls):
-
-        return cls.__redis_connect.keys(pattern='*')
+def get_redis_client():
+    try:
+        redis_client = redis.Redis(host=settings.redis_host, port=settings.redis_port, db=settings.redis_db)
+        return redis_client
+    except redis.exceptions.ConnectionError as e:
+        print(f"Ошибка подключения к Redis: {e}")
+        return None
