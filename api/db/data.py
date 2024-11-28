951fe6b7e2a45cd439c59f091839ee79c5c5db30
sskywwalkerr <radmir_rysaev1@mail.ru>
2024-11-28 18:14:02 +0500

    AsyncSession

---
+++ api/db/data.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlmodel import SQLModel
+from api.config import Config
+
+async_engine = create_async_engine(Config.DATABASE_URL, echo=True)
+
+
+async def init_db() -> None:
+    async with async_engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+
+async def get_session() -> AsyncSession:
+    Session = sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
+    async with Session() as session:
+        yield session
