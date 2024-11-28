caabc74a5c97a8d66e0097b0685df7d4abd55b24
sskywwalkerr <radmir_rysaev1@mail.ru>
2024-11-28 18:11:19 +0500

    run api

---
+++ api/db/main.py
@@ -0,0 +1,17 @@
+import uvicorn
+from fastapi import FastAPI, APIRouter
+from api.auth.routes import auth_router
+from api.db.data import init_db, get_session  # Импортируйте функции из db.py
+
+app = FastAPI(title="API Project")
+
+main_api_router = APIRouter()
+main_api_router.include_router(auth_router, prefix="/user", tags=["user"])
+app.include_router(main_api_router)
+
+@app.on_event("startup")
+async def on_startup() -> None:
+    await init_db()
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
