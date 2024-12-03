2c49fb9648d3bfa97d0430ab7ae48a457016f601
sskywwalkerr <radmir_rysaev1@mail.ru>
2024-12-03 18:42:54 +0500

    app

--- api/db/main.py
+++ api/db/main.py
@@ -1,7 +1,10 @@
 import uvicorn
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, BackgroundTasks
 from api.auth.routes import auth_router
-from api.db.data import init_db, get_session  # Импортируйте функции из db.py
+from api.db.data import init_db
+
+# from api.config import MailBody
+# from api.mail import send_mail
 
 app = FastAPI(title="API Project")
 
@@ -9,9 +12,17 @@ main_api_router = APIRouter()
 main_api_router.include_router(auth_router, prefix="/user", tags=["user"])
 app.include_router(main_api_router)
 
+
 @app.on_event("startup")
 async def on_startup() -> None:
     await init_db()
+
+# @app.post("/send-email")
+# def schedule_mail(req: MailBody, tasks: BackgroundTasks):
+#     data = req.dict()
+#     tasks.add_task(send_mail, data)
+#     return {"status": 200, "message": "email has been scheduled"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
