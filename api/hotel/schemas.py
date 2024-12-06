24ff1a79f368d03c428c22c4a206e7602288a651
sskywwalkerr <radmir_rysaev1@mail.ru>
2024-12-06 12:11:53 +0500

    schemas Hotel

---
+++ api/hotel/schemas.py
@@ -0,0 +1,35 @@
+import uuid
+from datetime import date, datetime
+from typing import List
+
+from pydantic import BaseModel
+
+
+class Hotel(BaseModel):
+    uid: uuid.UUID
+    name: str
+    location: str
+    description: str
+    rating: float
+    rooms: str
+    added_at: datetime
+    update_at: datetime
+
+# class HotelDetailModel(BaseModel):
+
+
+class HotelCreateModel(BaseModel):
+    name: str
+    location: str
+    description: str
+    rating: float
+    rooms: str
+    published_date: str
+
+
+class HotelUpdateModel(BaseModel):
+    name: str
+    location: str
+    description: str
+    rating: float
+    rooms: str
