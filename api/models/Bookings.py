2b13b8b47e60d06cd152ab77f64553ac5a7e2e2d
sskywwalkerr <radmir_rysaev1@mail.ru>
2024-12-16 19:17:35 +0500

    новая модель для бронирования

--- api/models/Bookings.py
+++ api/models/Bookings.py
@@ -1,13 +1,14 @@
 from datetime import datetime
 
-from sqlalchemy import Column, DateTime, Float, ForeignKey, String, Integer, Computed, Date
+from sqlalchemy import Column, ForeignKey, Integer, Computed, Date
 from sqlalchemy.dialects.postgresql import UUID as PGUUID
-from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import relationship, Mapped, mapped_column
-from sqlalchemy.sql import func
 from uuid import uuid4
 
 from api.database.db import Base
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from api.models import User, Room
 
 
 class Booking(Base):
@@ -31,8 +32,10 @@ class Booking(Base):
     room_uid = Column(PGUUID(as_uuid=True), ForeignKey("room.uid"))
     user_uid = Column(PGUUID(as_uuid=True), ForeignKey("users.uid"))
 
-    # user = relationship("User", back_populates="booking")
+    # user = relationship("User", back_populates="bookings")
     # room = relationship("Room", back_populates="booking")
+    user: Mapped["User"] = relationship(back_populates="bookings")
+    room: Mapped["Room"] = relationship(back_populates="booking")
 
     def __str__(self):
         return f'Бронирование: uid - {self.uid}, c {self.date_from} по {self.date_to}'
