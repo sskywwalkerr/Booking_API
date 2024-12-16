2dbf0c3d6d1495bfee3a447ff0f51da382b0a9c8
sskywwalkerr <radmir_rysaev1@mail.ru>
2024-12-16 19:17:17 +0500

    исправил модели

---
+++ api/models/__init__.py
@@ -0,0 +1,13 @@
+from .Bookings import Booking
+from .Hotel import Hotel
+from .Room import Room
+from .User import User
+from .Review import Review
+
+__all__ = [
+    "Booking",
+    "Hotel",
+    "Room",
+    "User",
+    "Review",
+]
